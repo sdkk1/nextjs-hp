a34582c91ad2aa05f84af38d01dd219b0d1becf0
Keita <fast.break.baske03@gmail.com>
2022-04-30 17:07:50 +0900

    add: Blogページのベースを作成

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Blog: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Blog</title>
+      </Head>
+      <h1 className='text-3xl font-bold underline'>Blog</h1>
+    </>
+  )
+}
+
+export default Blog
